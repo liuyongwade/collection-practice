ea0a900ec7ebac3ffd631685dc1ddf0e9aa97816
liuyongwade <liuyongwade@163.com>
2018-11-11 23:50:52 +0800

    Set代码

--- src/main/java/com/liuyong/set/HashSetPractice.java
+++ src/main/java/com/liuyong/set/HashSetPractice.java
@@ -1,5 +1,11 @@
 package com.liuyong.set;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class HashSetPractice {
 
+	public static void method1() {
+		Set<String> set = new LinkedHashSet<String>();
+	}
 }
