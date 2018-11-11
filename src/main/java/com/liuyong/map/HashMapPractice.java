f898757f8589432d0e48770d53e0dd74df9084c4
liuyongwade <liuyongwade@163.com>
2018-11-12 00:03:49 +0800

    总结

--- src/main/java/com/liuyong/map/HashMapPractice.java
+++ src/main/java/com/liuyong/map/HashMapPractice.java
@@ -1,9 +1,15 @@
 package com.liuyong.map;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class HashMapPractice {
 	public static void method1() {
-		new HashMap<String, String>();
+		Map<String, String> hashMap = new HashMap<String, String>();
+		hashMap.put("小明", "11");
+		hashMap.put("小红", "22");
+		hashMap.put("小张", "33");
+		new TreeMap<String, String>();
 	}
 }
